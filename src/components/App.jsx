b536654711647efe75f9c7c98053d611f0a4f4ff
ps807 <66031843+ps807@users.noreply.github.com>
2020-10-08 08:17:17 +0200

    END

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [buttonBackgroundColor, setButtonBackgroundColor] = useState("white");
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleOnMouseOver() {
+    setButtonBackgroundColor("black");
+  }
+
+  function handleOnMouseOut() {
+    setButtonBackgroundColor("white");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        onMouseOver={handleOnMouseOver}
+        onMouseOut={handleOnMouseOut}
+        style={{ backgroundColor: buttonBackgroundColor }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
